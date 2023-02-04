f6b28d55833073b2bfd0e4368a3af26924f2ab75
amemichelle <54964331+amemichelle@users.noreply.github.com>
2023-02-04 13:18:42 -0800

    add transition to hover state

--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -1,13 +1,16 @@
 import "./Orders.scss";
 import Navbar from "../../components/Navbar/Navbar";
 import OrderSidebar from "../../components/OrderSidebar/OrderSidebar";
-
+import noOrders from "../../assets/noorders.svg";
 function Orders() {
   return (
     <>
       <section className="orders">
         <Navbar></Navbar>
         <OrderSidebar></OrderSidebar>
+        <div className="orders__empty">
+          <img src={noOrders} className="orders__empty-img" />
+        </div>
       </section>
     </>
   );
