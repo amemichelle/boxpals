709f38057e10694488dfaf96af58913ca62f423b
amemichelle <54964331+amemichelle@users.noreply.github.com>
2023-02-07 18:11:15 -0800

    create manage orders card

--- src/components/ManageCard/ManageCard.js
+++ src/components/ManageCard/ManageCard.js
@@ -1,13 +1,19 @@
 import "./ManageCard.scss";
 import shoppingBag from "../../assets/shoppingbag.svg";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 function ManageCard() {
+  const [isVisbible, setIsVisibile] = useState(false);
+
   return (
     <>
-      <div className="card" id="card-base">
-        <div className="card__flag" id="flag">
-          <img src={shoppingBag} className="card__flag-img" />
-        </div>
+      <div className="card">
+        <AnimatePresence>
+          <motion.div className="card__flag">
+            <img src={shoppingBag} className="card__flag-img" />
+          </motion.div>
+        </AnimatePresence>
         <h3 className="card__heading">osumekeys</h3>
       </div>
     </>
