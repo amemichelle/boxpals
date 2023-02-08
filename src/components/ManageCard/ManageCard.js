53300e0404cb8027bcfb31e4e85fcc59cfd3c0f4
amemichelle <54964331+amemichelle@users.noreply.github.com>
2023-02-07 18:41:16 -0800

    working on zigzags

--- src/components/ManageCard/ManageCard.js
+++ src/components/ManageCard/ManageCard.js
@@ -4,14 +4,26 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
 function ManageCard() {
-  const [isVisbible, setIsVisibile] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   return (
     <>
-      <div className="card">
+      <div
+        className="card"
+        onMouseEnter={() => setIsVisible(true)}
+        onMouseLeave={() => setIsVisible(false)}
+      >
         <AnimatePresence>
-          <motion.div className="card__flag">
+          <motion.div
+            initial={false}
+            animate={{ height: isVisible ? "50px" : 0 }}
+            className="card__flag"
+          >
             <img src={shoppingBag} className="card__flag-img" />
+            <motion.div className="zigzag-container">
+              <div className="zigzag"></div>
+              <div className="zigzag"></div>
+            </motion.div>
           </motion.div>
         </AnimatePresence>
         <h3 className="card__heading">osumekeys</h3>
